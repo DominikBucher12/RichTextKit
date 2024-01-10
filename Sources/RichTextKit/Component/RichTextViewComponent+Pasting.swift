9b39a697b7cf4eecbda9eb86b90fd8934c27cf69
Dominik Bucher <dbucher@paylocity.com>
2024-01-10 11:03:32 +0100

    Use latest changes on pasting images

--- Sources/RichTextKit/Component/RichTextViewComponent+Pasting.swift
+++ Sources/RichTextKit/Component/RichTextViewComponent+Pasting.swift
@@ -137,7 +137,9 @@ private extension RichTextViewComponent {
         let newLine = NSAttributedString(string: "\n", attributes: currentRichTextAttributes)
         let content = NSMutableAttributedString(attributedString: richText)
         guard let insertString = getAttachmentString(for: image) else { return }
+        content.insert(NSAttributedString(string: "\n"), at: index)
         
+        insertString.insert(newLine, at: insertString.length)
         insertString.addAttributes(currentRichTextAttributes, range: insertString.richTextRange)
         content.insert(insertString, at: index)
         
